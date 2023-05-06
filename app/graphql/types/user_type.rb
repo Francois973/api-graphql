d328df8f3b4aaa68093b1658f6ff52448c88840a
Francois973 <robino.francois973@gmail.com>
2023-05-06 15:00:29 +0200

    chore: get result of request done

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,12 +3,12 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :first_name, String
-    field :last_name, String
+    field :full_name, String
     field :address, String
     field :postcode, String
     field :city, String
     field :country, String
+    field :posts, [Types::PostType]
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
